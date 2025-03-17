2d229c3b07e2762b1560383b4a4b124829855230
Pavel <pavel.de-wavrechin@epitech.eu>
2025-03-17 22:03:17 +0100

    feat(semicolons): add semicolons support to the shell

--- src/commands/commands_manager.c
+++ src/commands/commands_manager.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "my_lib.h"
 #include "commands.h"
 #include "mysh.h"
@@ -25,28 +26,33 @@ static const my_builtins_t my_builtins_arr[] = {
     {NULL, NULL},
 };
 
-void handle_redirection(char *command)
+int handle_metacharacters(char *command, char ***envp, int *error_code)
 {
-    handle_simple_left_redirection(command);
-    handle_simple_right_redirection(command);
+    if (handle_semicolons(command, envp, error_code))
+        return 1;
     handle_double_right_redirection(command);
+    handle_simple_right_redirection(command);
+    handle_simple_left_redirection(command);
+    if (handle_pipes(command, envp, error_code))
+        return 1;
+    return 0;
 }
 
 exit_status_t analyse_command(char ***envp, char *command, int *error_code)
 {
     exit_status_t status = NORMAL;
+    int stdin_cpy = duplicate_file_descriptor(STDIN_FILENO);
+    int stdout_cpy = duplicate_file_descriptor(STDOUT_FILENO);
     char **commands = NULL;
     int exit_code = 0;
 
-    if (handle_semicolons(command, envp, error_code))
+    if (handle_metacharacters(command, envp, error_code))
         return status;
-    if (handle_pipes(command, envp, error_code))
-        return status;
-    handle_redirection(command);
     for (int i = 0; my_builtins_arr[i].builtins_name; i++)
         if (my_builtins_arr[i].f(envp, command, &status, error_code))
             return status;
     if (*error_code != 84)
         my_exec(envp, command, &status, error_code);
+    restore_stdin_stdout_fd(stdin_cpy, stdout_cpy);
     return NORMAL;
 }
--- src/utils/handle_semicolons.c
+++ src/utils/handle_semicolons.c
@@ -6,25 +6,28 @@
 */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include "my_lib.h"
 #include "commands.h"
 #include "utils.h"
 
 static void execute_semicolon(char ***envp, char *command, int *error_code)
 {
+    int stdin_cpy = dup(STDIN_FILENO);
+    int stdout_cpy = dup(STDOUT_FILENO);
     char **commands = my_str_to_word_array(command, ";");
 
-    for (size_t i = 0; commands[i]; i++) {
-        printf("%s\n", commands[i]);
-    }
     for (int i = 0; commands[i] != NULL; i++) {
         analyse_command(envp, commands[i], error_code);
+        dup2(stdout_cpy, STDOUT_FILENO);
     }
     free_2d_array_of_char(commands);
 }
 
 int handle_semicolons(char *command, char ***envp, int *error_code)
 {
+    int stdin_cpy = dup(STDIN_FILENO);
+
     if (my_strstr(command, ";")) {
         execute_semicolon(envp, command, error_code);
         return 1;
