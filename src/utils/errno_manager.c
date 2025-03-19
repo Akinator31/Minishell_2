151895ec86917365b5317293cac43c086d267ae6
Pavel <pavel.de-wavrechin@epitech.eu>
2025-03-19 08:07:49 +0100

    fix(errno_manager): incorrect error message when trying to execute a folder

--- src/utils/errno_manager.c
+++ src/utils/errno_manager.c
@@ -5,18 +5,31 @@
 ** errno_manager
 */
 
-#include <errno.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include "my_lib.h"
 
 void errno_manager(int exec_return, char **command_element)
 {
-    if (exec_return != 0) {
+    char *error_to_string = NULL;
+
+    if (exec_return != 0 && errno == ENOENT) {
         write(2, command_element[0], my_strlen(command_element[0]));
         write(2, ": Command not found.\n", 21);
         free_2d_array_of_char(command_element);
-        exit(1);
+        exit(EXIT_FAILURE);
+    } else if (exec_return != 0) {
+        error_to_string = strerror(errno);
+        write(2, command_element[0], my_strlen(command_element[0]));
+        write(2, ": ", 2);
+        write(2, error_to_string, my_strlen(error_to_string));
+        write(2, ".\n", 2);
+        free_2d_array_of_char(command_element);
+        exit(EXIT_FAILURE);
+    } else {
+        return;
     }
 }
